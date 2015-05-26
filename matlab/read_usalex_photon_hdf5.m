49883bf70ec6750cccb62212accb03a57ea71031
Antonino Ingargiola <tritemio@gmail.com>
2015-05-26 14:50:29 -0700

    Fix typos in matlab script.

--- matlab/read_usalex_photon_hdf5.m
+++ matlab/read_usalex_photon_hdf5.m
@@ -2,36 +2,35 @@
 % This script shows how to read us-ALEX data store in a Photon-HDF5 file.
 %
 
-filename = '../data/0023uLRpitc_NTP_20dT_0.5GndCl.hdf5'
+filename = '../data/0023uLRpitc_NTP_20dT_0.5GndCl.hdf5';
 
 
 %% Read the data
-timestamps = h5read(filename, '/photon_data/timestamps')
-timestamps_unit = h5read(filename, '/photon_data/timestamps_specs/timestamps_unit')
-detectors = h5read(filename, '/photon_data/detectors')
+timestamps = h5read(filename, '/photon_data/timestamps');
+timestamps_unit = h5read(filename, '/photon_data/timestamps_specs/timestamps_unit');
+detectors = h5read(filename, '/photon_data/detectors');
 
-donor_ch = h5read(filename, '/photon_data/measurement_specs.detectors_specs.spectral_ch1')
-acceptor_ch = h5read(filename, '/photon_data/measurement_specs.detectors_specs.spectral_ch2')
+donor_ch = h5read(filename, '/photon_data/measurement_specs/detectors_specs/spectral_ch1');
+acceptor_ch = h5read(filename, '/photon_data/measurement_specs/detectors_specs/spectral_ch2');
 
-alex_period = h5read(filename, '/photon_data/photon_data.measurement_specs.alex_period')
-donor_period = h5read(filename, '/photon_data.measurement_specs.alex_period_spectral_ch1')
-acceptor_period = h5read(filename, '/photon_data.measurement_specs.alex_period_spectral_ch2')
+alex_period = h5read(filename, '/photon_data/measurement_specs/alex_period');
+donor_period = h5read(filename, '/photon_data/measurement_specs/alex_period_spectral_ch1');
+acceptor_period = h5read(filename, '/photon_data/measurement_specs/alex_period_spectral_ch2');
 
 
 %% Print data summary
-fprintf('Number of photons: %d\n', size(timestamps))
-fprintf('Timestamps unit:   %.2e seconds\n', timestamps_unit)
-fprintf('Detectors:         %s\n', unique(detectors))
-fprintf('Donor CH: %d     Acceptor CH: %d\n', [donor_ch; acceptor_ch])
-fprintf('ALEX period: %d  \nDonor period: %s      Acceptor period: %s', [alex_period; donor_period; acceptor_period])
+fprintf('Number of photons: %d\n', size(timestamps));
+fprintf('Timestamps unit:   %.2e seconds\n', timestamps_unit);
+fprintf('Detectors:         %s\n', unique(detectors));
+fprintf('Donor CH: %d     Acceptor CH: %d\n', [donor_ch; acceptor_ch]);
+fprintf('ALEX period: %d  \nDonor period: %d , %d      Acceptor period: %d , %d\n', [alex_period; donor_period; acceptor_period]);
 
 %% Compute timestamp selections
-timestamps_donor = timestamps(detectors == donor_ch)
-timestamps_acceptor = timestamps(detectors == acceptor_ch)
+timestamps_donor = timestamps(detectors == donor_ch);
+timestamps_acceptor = timestamps(detectors == acceptor_ch);
 
-timestamps_mod = mod(timestamps, alex_period)
-donor_excitation = (timestamps_mod < donor_period(2) | (timestamps_mod > donor_period(1))
-acceptor_excitation = (timestamps_mod < acceptor_period(2)) & (timestamps_mod > acceptor_period(1))
-timestamps_Dex = timestamps(donor_excitation)
-timestamps_Aex = timestamps(acceptor_excitation)
-
+timestamps_mod = mod(timestamps, int64(alex_period));
+donor_excitation = (timestamps_mod < donor_period(2)) | (timestamps_mod > donor_period(1));
+acceptor_excitation = (timestamps_mod < acceptor_period(2)) & (timestamps_mod > acceptor_period(1));
+timestamps_Dex = timestamps(donor_excitation);
+timestamps_Aex = timestamps(acceptor_excitation);
